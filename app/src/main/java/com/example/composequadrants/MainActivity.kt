3f35fdf60cdbed6a904c26a0fb228991417fc15a
Dylan Johnson <dj2330@gmail.com>
2024-11-09 14:43:15 -0500

    Finished layout with composables in quadrant formation

--- app/src/main/java/com/example/composequadrants/MainActivity.kt
+++ app/src/main/java/com/example/composequadrants/MainActivity.kt
@@ -3,22 +3,17 @@ package com.example.composequadrants
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.annotation.FloatRange
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -29,132 +24,84 @@ import com.example.composequadrants.ui.theme.ComposeQuadrantsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ComposeQuadrantsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Quadrants(
-                        textOneA = stringResource(id = R.string.text_composable),
-                        textOneB = stringResource(id = R.string.text_description),
-                        textTwoA = stringResource(id = R.string.image_composable),
-                        textTwoB = stringResource(id = R.string.image_description),
-                        textThreeA = stringResource(id = R.string.row_composable),
-                        textThreeB = stringResource(id = R.string.row_description),
-                        textFourA = stringResource(id = R.string.column_composable),
-                        textFourB = stringResource(id = R.string.column_description),
-                        modifier = Modifier.padding(innerPadding)
-
-                    )
-                }
+                QuadrantLayout()
             }
         }
     }
 }
 
 @Composable
-fun Quadrants(
-    textOneA: String,
-    textOneB: String,
-    textTwoA: String,
-    textTwoB: String,
-    textThreeA: String,
-    textThreeB: String,
-    textFourA: String,
-    textFourB: String,
-    modifier: Modifier = Modifier,
-
-    ) {
+fun QuadrantLayout() {
     Column(
-        Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        Row(
-            modifier = modifier.weight(2f),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-
-        ) {
-            Text(
-                text = textOneA,
-                modifier = modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFEADDFF))
-                    .padding(16.dp),
-                fontWeight = FontWeight.Bold,
-
-
-                )
-            Text(
-                text = textOneB,
-                modifier = modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFEADDFF))
-                    .padding(16.dp),
-
-
-                )
-
-            Text(
-                text = textTwoA,
-                modifier = modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFD0BCFF))
-                    .padding(16.dp),
-                fontWeight = FontWeight.Bold
+        Row(modifier = Modifier.weight(1f)) {
+            QuadrantTemplate(
+                header = stringResource(R.string.text_composable),
+                body = stringResource(R.string.text_description),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
             )
-            Text(
-                text = textTwoB,
-                modifier = modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFD0BCFF))
-                    .padding(16.dp),
+            QuadrantTemplate(
+                header = stringResource(R.string.image_composable),
+                body = stringResource(R.string.image_description),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
             )
-
         }
-        Row(
-            modifier = modifier.weight(2f), verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = textThreeA,
-                modifier = modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFEADDFF))
-                    .padding(16.dp),
-                fontWeight = FontWeight.Bold
+        Row(modifier = Modifier.weight(1f)) {
+            QuadrantTemplate(
+                header = stringResource(R.string.row_composable),
+                body = stringResource(R.string.row_description),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
             )
-            Text(
-                text = textThreeB,
-                modifier = modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFEADDFF))
-                    .padding(16.dp),
+            QuadrantTemplate(
+                header = stringResource(R.string.column_composable),
+                body = stringResource(R.string.column_description),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
             )
-            Text(
-                text = textFourA,
-                modifier = modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFEADDFF))
-                    .padding(16.dp),
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = textFourB,
-                modifier = modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .background(color = androidx.compose.ui.graphics.Color(0xFFEADDFF))
-                    .padding(16.dp),
-            )
-
-
         }
+    }
+}
+
+@Composable
+fun QuadrantTemplate(
+    header: String,
+    body: String,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = header,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp),
+
+            )
+        Text(
+            text = body,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuadrantLayoutPreview() {
+    ComposeQuadrantsTheme {
+        QuadrantLayout()
     }
 }
